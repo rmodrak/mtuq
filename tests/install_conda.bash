444c4cf443aa014a361c2227f4a9ec5852caa7a2
Ryan Modrak <rmodrak@gmail.com>
2025-07-19 17:16:33 -0400

    Further CI test changes

--- tests/install_conda.bash
+++ tests/install_conda.bash
@@ -5,51 +5,41 @@
 # TESTS MTUQ INSTALLATION UNDER CONDA
 #
 
-
-#
-# mtuq root directory
-#
-MTUQ_PATH=$(dirname ${BASH_SOURCE[0]})/..
-
-
-#
-# check that the following versions and dependencies match 
-# mtuq/docs/install/env_conda.rst
-#
-PYTHON_VERSION=3
-
-
 #
 # path where conda will be installed
 #
 _CONDA_PATH_="$HOME/miniforge3"
 
 #
-# it appears that conda and mamba differ subtly in their input arguments,
-# and are not completely interchangeable
+# conda or mamba?
 # 
 _CONDA_EXE_='conda'
 
-
-# note that CONDA_EXE and CONDA_PATH are reserved by the conda distribution
-# itself, so we used _CONDA_EXE_ and _CONDA_PATH_
-
+#
+# NOTES
+#
+# - CONDA_EXE and CONDA_PATH are reserved by the conda distribution, so we use
+#   _CONDA_EXE_ and _CONDA_PATH_ instead
+#
+# - it appears that conda and mamba are not completely interchangeable
+#
+# - for example, conda supports silent installation through `always_yes`
+#   and `changeps` settings, but it appers mamba does not
+#
 
 
 function conda_install {
-    _CONDA_INSTALL_PATH_=$1
+    INSTALL_PATH=$1
 
     hash -r
-    wget -nv $(conda_url) -O miniforge.sh
-    bash miniforge.sh -b -f -p $_CONDA_INSTALL_PATH_
+    wget -nv $(miniconda_url) -O miniforge.sh
+    bash miniforge.sh -b -f -p $INSTALL_PATH
     hash -r
     rm miniforge.sh
 }
 
 
 function conda_update {
-    _CONDA_PATH_=$1
-
     $_CONDA_EXE_ config --set always_yes yes --set changeps1 no 
     $_CONDA_EXE_ update -q conda
     $_CONDA_EXE_ info -a
@@ -58,49 +48,77 @@ function conda_update {
 
 
 function os_string {
-    echo "$(uname -s)"
+    echo "$(uname -ms)"
 }
 
 
-function conda_url {
+function miniforge_url {
 case "$(os_string)" in
-   Darwin)
+   "Darwin arm64")
      URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh"
      ;;
-   Linux)
+   "Linux x86_64")
      URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
      ;;
    *)
-     exit -1
+     # OS not supported
+     URL=""
      ;;
 esac
 echo $URL
 }
 
-CONDA_URL=$(conda_url)
-if [[ -z $CONDA_URL ]]
+
+function miniconda_url {
+case "$(os_string)" in
+   "Darwin arm64")
+     URL="https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-arm64.sh"
+     ;;
+   "Linux x86_64")
+     URL="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+     ;;
+   *)
+     # OS not supported
+     URL=""
+     ;;
+esac
+echo $URL
+}
+
+echo "uname -ms: $(os_string)"
+
+
+CONDA_URL=$(miniforge_url)
+if [[ -z "$CONDA_URL" ]]
 then
-    echo "uname -s: $(os_string)"
     echo "installation not supported"
     exit -1
 fi
 
 
 #
+# mtuq root directory
+#
+MTUQ_PATH=$(dirname ${BASH_SOURCE[0]})/..
+
+
+#
 # installation tests begin now
 #
 
-# if any test fails from now on, stop immediately
+echo
+echo "See mtuq/tests/ for installation logs"
+echo
+
+
+# from now on, if anything fails, stop immediately
 set -e
 
 # verbose mode from now on
 set -x
 
-
-echo
-echo "See mtuq/tests/ for installation logs"
-echo
 cd $MTUQ_PATH
+
 
 echo "Installing latest version of conda"
 echo
@@ -109,6 +127,7 @@ source $_CONDA_PATH_/etc/profile.d/conda.sh
 conda_update $_CONDA_PATH_ >> tests/log0
 echo SUCCESS
 echo
+
 
 echo "Testing mtuq installation"
 $_CONDA_EXE_ env create -q --name env1 --file env.yaml > tests/log1
